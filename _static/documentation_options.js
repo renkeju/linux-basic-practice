ca0de4908be985a9e435f0d839677359985855fe
renkeju <renkeju@gmail.com>
2023-01-16 09:17:17 +0000

    Update documentation

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -3,6 +3,7 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: 'alpha',
     LANGUAGE: 'zh_CN',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
